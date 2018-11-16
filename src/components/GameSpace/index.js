bd0d278182a6ec2ae9b0f2fb311379a5ce968307
Joseph Case <josephcase@Josephs-MacBook-Pro.local>
2018-11-16 11:50:13 +0100

    added react hooks to setplaydimensions component

--- src/components/GameSpace/index.js
+++ src/components/GameSpace/index.js
@@ -1,41 +1,71 @@
-import React, { PureComponent } from 'react';
+import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 import { setPlayDimensions } from '../../redux/actions';
 import StyledGameSpace from './StyledGameSpace';
 import PropTypes from 'prop-types';
 
-export class GameSpace extends PureComponent {
-    constructor(props) {
-        super(props);
-        this.elementRef = React.createRef();
+// export class GameSpace extends PureComponent {
+//     constructor(props) {
+//         super(props);
+//         this.elementRef = React.createRef();
+//     }
+
+//     setPlayDimensions = () => {
+//         const { setPlayDimensions } = this.props;
+//         const width = this.elementRef.current.offsetWidth;
+//         const height = this.elementRef.current.offsetHeight;
+//         setPlayDimensions({width, height});
+//     }
+
+//     componentDidMount() {
+//         window.onresize = this.setPlayDimensions;
+//         this.setPlayDimensions();
+//     }
+//     componentWillUnmount() {
+//         window.onresize = null;
+//     }
+
+//     render() {
+//         return (
+//             <StyledGameSpace ref={this.elementRef} >
+//                 {this.props.children}
+//             </StyledGameSpace>
+//         )
+//     }
+// }
+
+const GameSpace = ({ setPlayDimensions, children }) => {
+    
+    const elemRef = React.createRef();
+
+    useSetPlayDimensions(elemRef, setPlayDimensions);
+
+    return (
+        <StyledGameSpace ref={elemRef} >
+            {children}
+        </StyledGameSpace>
+    )
+
+}
+
+const useSetPlayDimensions = (elemRef, setPlayDimensions) => {
+    const handleResize = () => {
+        const width = elemRef.current.offsetWidth;
+        const height = elemRef.current.offsetHeight;
+        setPlayDimensions({ width, height });
     }
 
-    setPlayDimensions = () => {
-        const { setPlayDimensions } = this.props;
-        const width = this.elementRef.current.offsetWidth;
-        const height = this.elementRef.current.offsetHeight;
-        setPlayDimensions({width, height});
-    }
-
-    componentDidMount() {
-        window.onresize = this.setPlayDimensions;
-        this.setPlayDimensions();
-    }
-    componentWillUnmount() {
-        window.onresize = null;
-    }
-
-    render() {
-        return (
-            <StyledGameSpace ref={this.elementRef} >
-                {this.props.children}
-            </StyledGameSpace>
-        )
-    }
+    useEffect(() => {
+        window.onresize = handleResize;
+        handleResize();
+        return () => {
+            window.onresize = null;
+        }
+    }, [elemRef, setPlayDimensions])
 }
 
 GameSpace.defaultProps = {
-    setPlayDimensions: () => {}
+    setPlayDimensions: () => { }
 }
 
 GameSpace.propTypes = {
